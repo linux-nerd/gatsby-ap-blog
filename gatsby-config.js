e6db544aa8a85a847a637a0602d7206be6e70f71
Abhishek Prakash <abhishek.prakash@izettle.com>
2020-03-25 17:18:50 +0100

    cleaned up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `My Thoughts`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Abhishek Prakash`,
+      summary: `who lives and works in Stockholm building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `A small platform to share my thoughts`,
+    siteUrl: `https://blog.abhishekprakash.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `linux_nerd`,
     },
   },
   plugins: [
